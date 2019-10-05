d6aa4a6c1034e735568ccd999267e1edc33eaeac
LimiDerjaj <56152431+LimiDerjaj@users.noreply.github.com>
2019-10-05 21:11:40 +0200

    classi istanziabili+switch

--- EsCalcolatrice/Class1.cs
+++ EsCalcolatrice/Class1.cs
@@ -6,10 +6,35 @@ using System.Threading.Tasks;
 
 namespace MyNameSpace
 {
-    public static class Class1
+    class Class1
     {
+
+        public int Op { get; set; }
+        public int N1 { get; set; }
+        public int N2 { get; set; }
+        public float DivN1 { get; set; }
+        public float DivN2 { get; set; }
+
+        public Class1(int n1, int n2)
+        {
+            this.N1 = n1;
+            this.N2 = n2;
+            this.DivN1 = n1;
+            this.DivN2 = n2;
+        }
+
+        /*
+        public Class1(float n1, float n2)
+        {
+            
+
+        }*/
+
+
+        //FUNZIONI CALCOLATRICE 
         public static int Sum(int a, int b)
         {
+
             int c = a + b;
             return c;
         }
@@ -19,6 +44,7 @@ namespace MyNameSpace
             int c = a - b;
             return c;
         }
+
 
         public static int Prod(int a, int b)
         {
--- EsCalcolatrice/Program.cs
+++ EsCalcolatrice/Program.cs
@@ -9,6 +9,7 @@ namespace EsCalcolatrice
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             int x;
@@ -21,7 +22,11 @@ namespace EsCalcolatrice
 
             Console.WriteLine("Benvenuto in MyCalculator");
 
-            do {
+            Class1 c1 = new Class1(0, 0);
+
+            //CICLO CON CONTROLLO SCELTA OPERAZIONE
+            do
+            {
                 do
                 {
                     Console.WriteLine("\r\nScegli l' operazione da eseguire per iniziare:\r\n\r\n1 = +\r\n2 = -\r\n3 = *\r\n4 = /\r\n");
@@ -33,141 +38,160 @@ namespace EsCalcolatrice
                     }
                 } while (!success || z < 1 || z > 4);
 
-                if (z == 1)
+
+
+                //if (z == 1)
+                switch (z)
                 {
-                    do
-                    {
-                        Console.WriteLine("---------------------------\r\nADDIZIONE");
-                        Console.WriteLine("---------------------------\r\n\r\nInserisci il primo numero e premi INVIO");
-                        string numero1 = Console.ReadLine();
-                        success = Int32.TryParse(numero1, out x);
-                        if (success == false)
+                    case 1:
+                        do
                         {
-                            Console.WriteLine("\r\nIl valore inserito non è corretto");
-                        }
-                    } while (success == false);
-
-                    do
-                    {
-                        Console.WriteLine("\r\nInserisci il secondo numero e premi INVIO");
-                        string numero2 = Console.ReadLine();
-                        success = Int32.TryParse(numero2, out y);
-                        if (success == false)
-                        {
-                            Console.WriteLine("\r\nIl valore inserito non è corretto");
-                        }
-                    } while (success == false);
-
-                    result = Class1.Sum(x, y);
-
-                    Console.WriteLine("\r\nLa somma di " + x + " + " + y + " è: " + result + "\r\n\r\n---------------------------");
-
-                    //Console.ReadLine();
-                }
-
-
-                if (z == 2)
-                {
-                    do
-                    {
-                        Console.WriteLine("---------------------------\r\nSOTTRAZIONE");
-                        Console.WriteLine("---------------------------\r\n\r\nInserisci il primo numero e premi INVIO");
-                        string numero1 = Console.ReadLine();
-                        success = Int32.TryParse(numero1, out x);
-                        if (success == false)
-                        {
-                            Console.WriteLine("\r\nIl valore inserito non è corretto");
-                        }
-                    } while (success == false);
-
-                    do
-                    {
-                        Console.WriteLine("\r\nInserisci il secondo numero e premi INVIO");
-                        string numero2 = Console.ReadLine();
-                        success = Int32.TryParse(numero2, out y);
-                        if (success == false)
-                        {
-                            Console.WriteLine("\r\nIl valore inserito non è corretto");
-                        }
-                    } while (success == false);
-
-                    result = Class1.Diff(x, y);
-
-                    Console.WriteLine("\r\nLa differenza di " + x + " - " + y + " è: " + result + "\r\n\r\n---------------------------");
-                    //Console.ReadLine();
-
-                }
-
-                if (z == 3)
-                {
-                    do
-                    {
-                        Console.WriteLine("---------------------------\r\nMOLTIPLICAZIONE");
-                        Console.WriteLine("---------------------------\r\n\r\nInserisci il primo numero e premi INVIO");
-                        string numero1 = Console.ReadLine();
-                        success = Int32.TryParse(numero1, out x);
-                        if (success == false)
-                        {
-                            Console.WriteLine("\r\nIl valore inserito non è corretto");
-                        }
-                    } while (success == false);
-
-                    do
-                    {
-                        Console.WriteLine("\r\nInserisci il secondo numero e premi INVIO");
-                        string numero2 = Console.ReadLine();
-                        success = Int32.TryParse(numero2, out y);
-                        if (success == false)
-                        {
-                            Console.WriteLine("\r\nIl valore inserito non è corretto");
-                        }
-                    } while (success == false);
-
-                    result = Class1.Prod(x, y);
-
-                    Console.WriteLine("\r\nIl prodotto di " + x + " * " + y + " è: " + result + "\r\n\r\n---------------------------");
-                    //Console.ReadLine();
-                }
-
-                if (z == 4)
-                {
-                    do
-                    {
-                        Console.WriteLine("---------------------------\r\nDIVISIONE");
-                        Console.WriteLine("---------------------------\r\n\r\nInserisci il primo numero e premi INVIO");
-                        string numero1 = Console.ReadLine();
-                        success = Int32.TryParse(numero1, out x);
-                        if (!success)
-                        {
-                            Console.WriteLine("\r\nIl valore inserito non è corretto");
-                        }
-                    } while (success == false);
-
-                    do
-                    {
-                        Console.WriteLine("\r\nInserisci il secondo numero e premi INVIO");
-                        string numero2 = Console.ReadLine();
-                        success = Int32.TryParse(numero2, out y);
-                        if (!success)
-                        {
-                            Console.WriteLine("\r\nIl valore inserito non è corretto, inserire un numero intero");
-                        }
-                        else
-                            if (y==0)
+                            Console.WriteLine("---------------------------\r\nADDIZIONE");
+                            Console.WriteLine("---------------------------\r\n\r\nInserisci il primo numero e premi INVIO");
+                            string numero1 = Console.ReadLine();
+                            success = Int32.TryParse(numero1, out x);
+                            c1.N1 = x;
+                            if (success == false)
                             {
-                            Console.WriteLine("\r\nImpossibile eseguire una divisione per 0");
+                                Console.WriteLine("\r\nIl valore inserito non è corretto");
                             }
-                    } while (!success || y==0);
+                        } while (success == false);
 
-                    result1 = Class1.Div(x, y);
+                        do
+                        {
+                            Console.WriteLine("\r\nInserisci il secondo numero e premi INVIO");
+                            string numero2 = Console.ReadLine();
+                            success = Int32.TryParse(numero2, out y);
+                            c1.N2 = y;
+                            if (success == false)
+                            {
+                                Console.WriteLine("\r\nIl valore inserito non è corretto");
+                            }
+                        } while (success == false);
 
-                    Console.WriteLine("\r\nLa divisione tra " + x + " / " + y + " è: " + result1+ "\r\n\r\n---------------------------");
-                    //Console.ReadLine();
+                        result = Class1.Sum(x, y);
+
+                        Console.WriteLine("\r\nLa somma di " + c1.N1 + " + " + c1.N2 + " è: " + result + "\r\n\r\n---------------------------");
+
+                        Console.ReadLine();
+
+                        break;
+
+
+                    //if (z == 2)
+                    //{
+                    case 2:
+                        do
+                        {
+                            Console.WriteLine("---------------------------\r\nSOTTRAZIONE");
+                            Console.WriteLine("---------------------------\r\n\r\nInserisci il primo numero e premi INVIO");
+                            string numero1 = Console.ReadLine();
+                            success = Int32.TryParse(numero1, out x);
+                            c1.N1 = x;
+                            if (success == false)
+                            {
+                                Console.WriteLine("\r\nIl valore inserito non è corretto");
+                            }
+                        } while (success == false);
+
+                        do
+                        {
+                            Console.WriteLine("\r\nInserisci il secondo numero e premi INVIO");
+                            string numero2 = Console.ReadLine();
+                            success = Int32.TryParse(numero2, out y);
+                            c1.N2 = y;
+                            if (success == false)
+                            {
+                                Console.WriteLine("\r\nIl valore inserito non è corretto");
+                            }
+                        } while (success == false);
+
+                        result = Class1.Diff(x, y);
+
+                        Console.WriteLine("\r\nLa differenza di " + c1.N1 + " - " + c1.N2 + " è: " + result + "\r\n\r\n---------------------------");
+                        //Console.ReadLine();
+                        break;
+                    //}
+
+                    //if (z == 3)
+                    //{
+                    case 3:
+                        do
+                        {
+                            Console.WriteLine("---------------------------\r\nMOLTIPLICAZIONE");
+                            Console.WriteLine("---------------------------\r\n\r\nInserisci il primo numero e premi INVIO");
+                            string numero1 = Console.ReadLine();
+                            success = Int32.TryParse(numero1, out x);
+                            c1.N1 = x;
+                            if (success == false)
+                            {
+                                Console.WriteLine("\r\nIl valore inserito non è corretto");
+                            }
+                        } while (success == false);
+
+                        do
+                        {
+                            Console.WriteLine("\r\nInserisci il secondo numero e premi INVIO");
+                            string numero2 = Console.ReadLine();
+                            success = Int32.TryParse(numero2, out y);
+                            c1.N2 = y;
+                            if (success == false)
+                            {
+                                Console.WriteLine("\r\nIl valore inserito non è corretto");
+                            }
+                        } while (success == false);
+
+                        result = Class1.Prod(x, y);
+
+                        Console.WriteLine("\r\nIl prodotto di " + c1.N1 + " * " + c1.N2 + " è: " + result + "\r\n\r\n---------------------------");
+                        //Console.ReadLine();
+                        //}
+                        break;
+
+                    //if (z == 4)
+                    //{
+                    case 4:
+                        do
+                        {
+                            Console.WriteLine("---------------------------\r\nDIVISIONE");
+                            Console.WriteLine("---------------------------\r\n\r\nInserisci il primo numero e premi INVIO");
+                            string numero1 = Console.ReadLine();
+                            success = Int32.TryParse(numero1, out x);
+                            c1.DivN1 = x;
+                            if (!success)
+                            {
+                                Console.WriteLine("\r\nIl valore inserito non è corretto");
+                            }
+                        } while (success == false);
+
+                        do
+                        {
+                            Console.WriteLine("\r\nInserisci il secondo numero e premi INVIO");
+                            string numero2 = Console.ReadLine();
+                            success = Int32.TryParse(numero2, out y);
+                            c1.DivN2 = y;
+                            if (!success)
+                            {
+                                Console.WriteLine("\r\nIl valore inserito non è corretto, inserire un numero intero");
+                            }
+                            else
+                                if (y == 0)
+                            {
+                                Console.WriteLine("\r\nImpossibile eseguire una divisione per 0");
+                            }
+                        } while (!success || y == 0);
+
+                        result1 = Class1.Div(x, y);
+
+                        Console.WriteLine("\r\nLa divisione tra " + c1.DivN1 + " / " + c1.DivN2 + " è: " + result1 + "\r\n\r\n---------------------------");
+                        //Console.ReadLine();
+                        break;
                 }
 
-            Console.WriteLine("\r\nPremere qualsiasi tasto per continuare, n per uscire, ");
-            a=Console.ReadLine();
-            } while ( a != "n");            
-         } 
+                Console.WriteLine("\r\nPremere qualsiasi tasto per continuare, n per uscire, ");
+                a = Console.ReadLine();
+            } while (a != "n");
+        }
     }
 }
+
